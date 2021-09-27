ed7a2f970d34f0318e81e2b5262f4b4c253e3a49
pedrohenriquecardoso <pedrohcardoso.1995@gmail.com>
2021-09-26 23:56:39 -0300

    route params in C view

--- src/views/ViewC.js
+++ src/views/ViewC.js
@@ -2,7 +2,8 @@ import React from 'react'
 import CentralText from '../components/CentralText'
 
 export default props => {
+    const route = props.route || { params: {numero: 0}}
     return (
-        <CentralText colorBackground='#9932cd'>View C</CentralText>
+        <CentralText colorBackground='#9932cd'>View C - {route.params.numero}</CentralText>
     )
 }
