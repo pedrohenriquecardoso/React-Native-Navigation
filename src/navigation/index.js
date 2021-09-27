23e6fcaf93cac744bfee27f266e929fc9b350db6
pedrohenriquecardoso <pedrohcardoso.1995@gmail.com>
2021-09-27 00:10:12 -0300

    tab navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import {SafeAreaView} from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
-import Stack from './Stack'
+//import Stack from './Stack'
+import Tab from './Tab'
 
 export default props => {
     return (
         <SafeAreaView style={{flex: 1}}>
             <NavigationContainer>
-                <Stack />
+                {/* <Stack /> */}
+                <Tab />
             </NavigationContainer>
         </SafeAreaView>
     )
